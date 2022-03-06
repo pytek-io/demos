edd7d9885849e525356cd6a92af050a84925600b
fdv1 <francois_du_vignaud@yahoo.com>
2022-03-06 11:02:02 +0000

    fixed edit link title

--- ant/demos/data_entry/time-picker/basic.py
+++ ant/demos/data_entry/time-picker/basic.py
@@ -1,13 +1,13 @@
 from datetime import time
 
 from reflect import autorun
-from reflect_antd import TimePicker, DatePicker
+from reflect_antd import TimePicker
 from reflect_html import *
 
 
 def app():
-    time_picker = DatePicker(
-        # defaultValue=time(0, 0, 0),
+    time_picker = TimePicker(
+        defaultValue=time(0, 0, 0),
     )
     autorun(lambda: print("changed", time_picker()))
     return time_picker
