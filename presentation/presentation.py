5b88d84c9522c350986c4ba741ac6c89dfe12f63
fdv1 <francois@pytek.io>
2022-03-22 21:42:50 +0000

    further improvements to presentation to early adopters

--- presentation/presentation.py
+++ presentation/presentation.py
@@ -1,28 +1,28 @@
-TITLE = "Early adopters presentation"
-from reflect import get_window, make_observable, ResponsiveValue
-from reflect_html import img, div, svg, path, a
-from website.common import BACKGROUND_COLOR, FONT_FAMILY, LIGHT_BLUE, GREEN
-from website.main import HTML_LINKS
-from website.home import SLOGAN
-from functools import partial
-from reflect_swiper import Swiper, SwiperSlide
-from website.reflect.gallery import MENU as GALLERY_MENU
-
 import itertools
 import os
+from functools import partial
 
+import yaml
+from reflect import ResponsiveValue, get_window, make_observable, CachedEvaluation
+from reflect_html import a, div, img, path, svg
+from reflect_swiper import Swiper, SwiperSlide
+from website.common import BACKGROUND_COLOR, FONT_FAMILY, GREEN, LIGHT_BLUE
+from website.home import SLOGAN
+from website.main import HTML_LINKS
+from website.reflect.gallery import MENU as GALLERY_MENU
+
+TITLE = "Early adopters presentation"
+BODY_STYLE = {"backgroundColor": "rgb" + str(BACKGROUND_COLOR), "fontSize": 10}
+CSS = ["test.css"]
 RIGHT_ARROW = 39
 LEFT_ARROW = 37
 LOGO_HEIGHT = 52
+BOTTOM_HEIGHT = 52
 HOME_PAGE = "website.home"
 
+
 maximize = "M396.795 396.8H320V448h128V320h-51.205zM396.8 115.205V192H448V64H320v51.205zM115.205 115.2H192V64H64v128h51.205zM115.2 396.795V320H64v128h128v-51.205z"
-
 minimize = "M64 371.2h76.795V448H192V320H64v51.2zm76.795-230.4H64V192h128V64h-51.205v76.8zM320 448h51.2v-76.8H448V320H320v128zm51.2-307.2V64H320v128h128v-51.2h-76.8z"
-
-
-BODY_STYLE = {"backgroundColor": "rgb" + str(BACKGROUND_COLOR), "fontSize": 10}
-CSS = ["test.css"]
 
 
 def select_file_extension(file_path):
@@ -32,7 +32,7 @@ def select_file_extension(file_path):
             return maybe_path
 
 
-def title(content, color=LIGHT_BLUE, fontSize="40px"):
+def title(content, color=LIGHT_BLUE, fontSize="2rem"):
     return div(
         content,
         style={
@@ -50,65 +50,6 @@ def add_dicts(*dicts):
     for d in dicts:
         result.update(d.items())
     return result
-
-
-content = [
-    [
-        "Who are we?",
-        "A startup building a Python enterprise framework called Reflect.",
-        "Francois du Vignaud has launched the project in November 2021 with the help of a couple friends and ex colleagues. We strongly believe that we can provide a value          have very ambitious goals for the near future."
-    ],
-    [
-        "What do we mean by Python enterprise framework?",
-        "A technology allowing to build web applications (aka web apps) all in Python.",
-        "Python has established itself as the most popular programming language after three decades of existence. Beyond scientific applications it is an extremely efficient programming language thanks to its user friendliness and humongous ecosystem of libraries. But despite all this there isn't any standard enterprise framework making for building enterprise system."
-    ],
-    [
-        "What is a web app?",
-        "A web page which allows for user interaction (eg: web mail, social network pages, etc).",
-        "Put it another way a web app is a page which depends on the current context be it user inputs, realtime data, etc."
-    ],
-    [
-        "What are Reflect's advantages over existing solutions?",
-        "Web apps require complex infrastructure, various programming languages, tools, etc. With Reflect the only thing you need to do is write (short) Python scripts.",
-        "The equivalent solutions to Reflect are either NodeJS and MS Blazer but for the fact that they rely on JavaScript and .Net respectively. Short of that one has to build a significant amount of technical infrastructure which usually comprise a front end part writtent in JS and a back-end part written in the target language. Building this is a challenging engineering issue which is usually completely uncorrelated with the actual business."
-    ],
-    [
-        "In which situation can you use Reflect?",
-        "Reflect is very well suited for building in house systems, and B2B portals, etc",
-        "Given the architectural choices Reflect makes building such apps extremely easy. Any mildly experience Python can build a web app in a couple of hours. Deploying and maintaining is fully automated, making the overall development process perfectly seameless."
-    ],
-    [
-        "Are there any examples of apps build on top of Reflect?",
-        "You can see a few in the website app demos section. Our website and even this presentation are build with Reflect (we eat our own dog food and we love it!).",
-        "We are building prototypes for a few startups already, some of them should be made public soon."
-    ],
-    [
-        "Which industry would benefit most from Reflect?",
-        "Finance is the most obvious, but no mean the only one, industry, services, etc. Any organization which need an efficient solution to build systems in Python.",
-        ""
-    ],
-    [
-        "What is our goal?",
-        "Expand our user community to the professional world. Gather users feedback while helping them to get a better understanding of their needs.",
-    ],
-    [
-        "Which users are we targeting?",
-        "Any startup, small company that has a business need for Reflect.",
-    ],
-    [
-        "How much does Reflect cost?",
-        "Nothing, Reflect is free to use for the time being. Early adopters will have the right to use their copy indefinitely.",
-    ],
-    [
-        "How do you get started?",
-        "You can download Reflect and start using it straightaway. Please feel to reach out to us we will be more than happy to guide your first steps!",
-    ],
-    [
-        "Anything else we want you to know?",
-        "Reflect is in beta, meaning we won't change its interface. We are aiming for the prod release mid year. We are also working hard on improving the tutorial, demos, so stay tuned.",
-    ],
-]
 
 
 def roman_numeral(number):
@@ -175,12 +116,116 @@ def create_bullet(color="white", transparent=True, onClick=None):
     return div(None, size=10, style=STYLE, onClick=onClick)
 
 
+def create_answer_box(answer, details, detail_level):
+    update_detail_level = lambda: detail_level.set(1 if detail_level() == 2 else 2)
+    return div(
+        div(
+            [
+                div(
+                    lambda: answer + ("" if detail_level() == 2 else ".."),
+                    onClick=update_detail_level,
+                ),
+                lambda: div(
+                    details,
+                    onClick=update_detail_level,
+                )
+                if detail_level() == 2
+                else None,
+            ]
+        ),
+        style={
+            "marginRight": "10px",
+            "marginBottom": "20px",
+            "fontSize": "1.5rem",
+            "color": LIGHT_BLUE,
+            "borderColor": GREEN,
+            "borderStyle": "dashed",
+            "padding": 10,
+            "display": "inline-block",
+            "pointerEvents": "all",
+            "cursor": "pointer",
+            "textAlign": "justify",
+        },
+    )
+
+
+def create_question_and_answer(
+    default_detail_level_value, on_drill_down, question, answer, details
+):
+    detail_level = make_observable(default_detail_level_value)
+
+    def drill():
+        on_drill_down(detail_level)
+        detail_level.set(
+            default_detail_level_value
+            + (0 if detail_level() != default_detail_level_value else 1)
+        )
+
+    return div(
+        [
+            div(
+                question,
+                onClick=drill,
+                style={
+                    "fontFamily": FONT_FAMILY,
+                    "color": GREEN,
+                    "fontSize": "2rem",
+                    "padding": 10,
+                    "pointerEvents": "all",
+                    "cursor": "pointer",
+                },
+            ),
+            lambda: create_answer_box(answer, details, detail_level)
+            if detail_level() > 0
+            else None,
+        ]
+    )
+
+
+def create_page(items, default_detail_level_value):
+    current_detail_level = None
+
+    def on_drill_down(new_detail_level):
+        nonlocal current_detail_level
+        if (
+            current_detail_level is not None
+            and current_detail_level is not new_detail_level
+        ):
+            current_detail_level.set(default_detail_level_value)
+        current_detail_level = new_detail_level
+
+    return div(
+        [
+            create_question_and_answer(default_detail_level_value, on_drill_down, *item)
+            for item in items
+        ],
+        style={
+            # "maxHeight": f"calc(100vh - {BOTTOM_HEIGHT + LOGO_HEIGHT}px)",
+            # "overflowY": "scroll",
+            "paddingTop": 10
+        },
+    )
+
+
 def app():
+    content = yaml.safe_load(
+        open("demos/presentations/early_adopters.yaml", "r").read()
+    )
     window = get_window()
     full_screen = make_observable(False)
+    details_level = make_observable(0)
+    resolution = window.width() * window.height() / 1000
+    print(resolution)
+    values = (5, 3, 2)
+    if resolution < 300.0:
+        values = (3, 2, 1)
+    elif resolution > 800:
+        window.update_tag_style([("font-size", "24px")], "html")
+
+    NB_QUESTIONS_PER_PAGE = dict(enumerate(values))
 
     def page_index():
-        return int(window.hash()) if window.hash() else 0
+        return min(int(window.hash()) if window.hash() else 0, len(slides()) - 1)
 
     def set_page_index(index):
         window.hash.set(str(index))
@@ -189,7 +234,7 @@ def app():
         nonlocal page_index, set_page_index
         page_index_value = page_index()
         if increment:
-            if page_index_value + 1 < len(slides):
+            if page_index_value + 1 < len(slides()):
                 set_page_index(page_index_value + 1)
         else:
             if page_index_value > 0:
@@ -240,53 +285,21 @@ def app():
             title(
                 "Early adopters presentation",
                 color=GREEN,
-                fontSize="3rem",
+                fontSize="1.5rem",
             ),
         ],
     )
 
-    slides = [main_page] + [
-        div(
-            # [title(f"Rapid fire questions and answers {roman_numeral(index)}")]
-            # +
-            list(
-                itertools.chain.from_iterable(
-                    (
-                        div(
-                            question,
-                            style={
-                                "fontFamily": FONT_FAMILY,
-                                "color": GREEN,
-                                "fontSize": "2rem",
-                                "padding": 10,
-                            },
-                        ),
-                        div(
-                            answer,
-                            style={
-                                "marginRight": "10px",
-                                "marginBottom": "20px",
-                                "fontSize": "2rem",
-                                "color": LIGHT_BLUE,
-                                "borderColor": GREEN,
-                                "borderStyle": "dashed",
-                                "padding": 10,
-                                "display": "inline-block",
-                                "width": "100%",
-                            },
-                        ),
-                    )
-                    for question, answer in items
-                )
-            ),
-        )
-        for index, items in enumerate(grouper(3, content), 1)
-    ]
+    def generate_slides():
+        details_level_value = details_level()
+        return [main_page] + [
+            create_page(items, details_level_value)
+            for items in grouper(NB_QUESTIONS_PER_PAGE[details_level_value], content)
+        ]
 
-    def update_full_screen():
-        window.update_full_screen(not full_screen())
+    slides = CachedEvaluation(generate_slides)
 
-    icon = create_icon(
+    full_screen_icon = create_icon(
         lambda: minimize if full_screen() else maximize,
         fill="white",
         width="2.5vh",
@@ -295,10 +308,16 @@ def app():
             "pointerEvents": "all",
             "cursor": "pointer",
         },
-        onClick=update_full_screen,
+        onClick=lambda: window.update_full_screen(not full_screen()),
     )
 
-    def create_bullets():
+    detail_level_icon = img(
+        src=lambda: f"reflect/static/menu-icon-{details_level() + 1}.svg",
+        style=dict(width="1.6vh", pointerEvents="all", cursor="pointer"),
+        onClick=lambda: details_level.set((details_level() + 1) % 3),
+    )
+
+    def page_bullets():
         page_index_value = page_index()
         return [
             create_bullet(
@@ -306,15 +325,11 @@ def app():
                 color="white",
                 onClick=partial(set_page_index, index),
             )
-            for index in range(len(slides))
+            for index in range(len(slides()))
         ]
 
     bottom = div(
-        left_center_right(
-            icon,
-            create_bullet(transparent=True, color="white"),
-            create_bullets,
-        ),
+        left_center_right(full_screen_icon, detail_level_icon, page_bullets),
         style={
             "display": "flex",
             "flexDirection": "column",
@@ -327,7 +342,7 @@ def app():
 
     def responsive_margins(content, style):
         margin = ResponsiveValue(
-            xs=10, sm="10vh", md="10vh", lg=None, xl=None, xxl="20vh"
+            xs=10, sm="10vh", md="10vh", lg=None, xl=None, xxl="30vh"
         )
         return div(
             content,
@@ -349,13 +364,14 @@ def app():
                             "marginTop": "1rem",
                         },
                     ),
-                    href="#" + HOME_PAGE,
+                    href="https://pytek.io",
+                    target="_blank",
                 ),
                 style={"height": LOGO_HEIGHT},
             ),
             responsive_margins(
                 div(
-                    lambda: slides[page_index()],
+                    lambda: slides()[page_index()],
                     style={
                         "display": "flex",
                         "flexDirection": "column",
@@ -363,16 +379,14 @@ def app():
                         "height": "100%",
                     },
                 ),
-                style={
-                    "flex": 1,
-                },
+                style={"flex": 1},
             ),
             # dummy bottom div to ensure the main one is centered
-            div(None, style={"height": "100px"}),
+            div(None, style={"height": BOTTOM_HEIGHT}),
             div(
                 bottom,
                 style={
-                    "height": "100px",
+                    "height": BOTTOM_HEIGHT,
                     "width": "100%",
                     "position": "absolute",
                     "bottom": 0,
