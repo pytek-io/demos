3d5be89f03129f696eb801a97b0ac82fc7f6e236
fdv1 <francois@pytek.io>
2022-05-18 13:28:16 +0100

    transfer basic example fixed

--- ant/demos/data_entry/transfer/basic.py
+++ ant/demos/data_entry/transfer/basic.py
@@ -1,22 +1,15 @@
-from reflect_html import *
+from reflect import autorun, js
 from reflect_antd import Transfer
-from reflect import js
-mockData = [
-    {
-        "key": i,
-        "title": f"content{i + 1}",
-        "description": f"description of content{i + 1}",
-    }
-    for i in range(20)
-]
 
-initialTargetKeys = [item["key"] for item in mockData if item["key"] > 10]
+mockData = [{"key": i, "title": f"content{i + 1}"} for i in range(10)]
+
 
 def app():
-    return Transfer(dataSource=mockData, 
-    titles=['Source', 'Target'], 
-    selectedKeys=selectedKeys, 
-    onChange=onChange, 
-    onSelectChange=onSelectChange, 
-    onScroll=onScroll, 
-    render=js("fetch_attribute", "title"))
+    result = Transfer(
+        dataSource=mockData,
+        titles=["Source", "Target"],
+        defaultTargetKeys=[item["key"] for item in mockData if item["key"] % 2 == 0],
+        render=js("fetch_attribute", "title"),
+    )
+    autorun(lambda: print("target", result()))
+    return result
