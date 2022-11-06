74930ed25a6dc156c4873e5a0aee93de5ebdacbd
fdv1 <francois@pytek.io>
2022-11-06 11:03:56 +0000

    fixed checkbox controller demo

--- ant/demos/data_entry/checkbox/controller.py
+++ ant/demos/data_entry/checkbox/controller.py
@@ -12,27 +12,29 @@ def app():
         checked=checked,
         disabled=disabled,
     )
-    autorun(lambda: print(f"{check_box()=}"))
-    return [
-        p(
-            check_box,
-            style=dict(marginBottom="20px"),
-        ),
-        p(
-            [
-                Button(
-                    lambda: "Check" if checked() else "Uncheck",
-                    type="primary",
-                    size="small",
-                    onClick=toggle_observable(check_box.checked),
-                ),
-                Button(
-                    lambda: "Enable" if disabled() else "Disable",
-                    style=dict(margin="0 10px"),
-                    type="primary",
-                    size="small",
-                    onClick=toggle_observable(disabled),
-                ),
-            ]
-        ),
-    ]
+    autorun(lambda: print(f"{check_box()}"))
+    return div(
+        [
+            p(
+                check_box,
+                style=dict(marginBottom="20px"),
+            ),
+            p(
+                [
+                    Button(
+                        lambda: "Check" if checked() else "Uncheck",
+                        type="primary",
+                        size="small",
+                        onClick=toggle_observable(checked),
+                    ),
+                    Button(
+                        lambda: "Enable" if disabled() else "Disable",
+                        style=dict(margin="0 10px"),
+                        type="primary",
+                        size="small",
+                        onClick=toggle_observable(disabled),
+                    ),
+                ]
+            ),
+        ]
+    )
