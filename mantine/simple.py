3bcef89bbdc651d01caf2bfc3e6c9bba69c4e8bd
fdv1 <francois@pytek.io>
2022-11-15 14:44:57 +0000

    minor fix

--- mantine/simple.py
+++ mantine/simple.py
@@ -3,7 +3,7 @@ import reflect_mantine as mantine
 
 
 def app():
-    name = mantine.TextInput()
+    name = mantine.TextInput(defaultValue="John")
     greeting = lambda: f"Hello {name()}"
     greeting_component = mantine.Text(greeting)
     return mantine.Group([name, greeting_component])
