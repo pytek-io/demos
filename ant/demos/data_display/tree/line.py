68aa164ae58f75f495b44240079f4b658f8bc62d
fdv1 <francois@pytek.io>
2023-02-02 22:27:49 +0000

    fixed ant tree line example

---
+++ ant/demos/data_display/tree/line.py
@@ -0,0 +1,120 @@
+import reflect_html as html
+import reflect_antd as antd
+import reflect_ant_icons as ant_icons
+
+treeData = [
+    {
+        "title": "parent 1",
+        "key": "0-0",
+        "icon": ant_icons.CarryOutOutlined(),
+        "children": [
+            {
+                "title": "parent 1-0",
+                "key": "0-0-0",
+                "icon": ant_icons.CarryOutOutlined(),
+                "children": [
+                    {
+                        "title": "leaf",
+                        "key": "0-0-0-0",
+                        "icon": ant_icons.CarryOutOutlined(),
+                    },
+                    {
+                        "title": [
+                            html.div("multiple line title"),
+                            html.div("multiple line title"),
+                        ],
+                        "key": "0-0-0-1",
+                        "icon": ant_icons.CarryOutOutlined(),
+                    },
+                    {
+                        "title": "leaf",
+                        "key": "0-0-0-2",
+                        "icon": ant_icons.CarryOutOutlined(),
+                    },
+                ],
+            },
+            {
+                "title": "parent 1-1",
+                "key": "0-0-1",
+                "icon": ant_icons.CarryOutOutlined(),
+                "children": [
+                    {
+                        "title": "leaf",
+                        "key": "0-0-1-0",
+                        "icon": ant_icons.CarryOutOutlined(),
+                    }
+                ],
+            },
+            {
+                "title": "parent 1-2",
+                "key": "0-0-2",
+                "icon": ant_icons.CarryOutOutlined(),
+                "children": [
+                    {
+                        "title": "leaf",
+                        "key": "0-0-2-0",
+                        "icon": ant_icons.CarryOutOutlined(),
+                    },
+                    {
+                        "title": "leaf",
+                        "key": "0-0-2-1",
+                        "icon": ant_icons.CarryOutOutlined(),
+                        # switcher"icon": <FormOutlined />,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        "title": "parent 2",
+        "key": "0-1",
+        "icon": ant_icons.CarryOutOutlined(),
+        "children": [
+            {
+                "title": "parent 2-0",
+                "key": "0-1-0",
+                "icon": ant_icons.CarryOutOutlined(),
+                "children": [
+                    {
+                        "title": "leaf",
+                        "key": "0-1-0-0",
+                        "icon": ant_icons.CarryOutOutlined(),
+                    },
+                    {
+                        "title": "leaf",
+                        "key": "0-1-0-1",
+                        "icon": ant_icons.CarryOutOutlined(),
+                    },
+                ],
+            },
+        ],
+    },
+]
+
+
+def app():
+    show_line = antd.Switch()
+    show_icon = antd.Switch()
+    return html.div(
+        [
+            html.div(
+                [
+                    "show line:",
+                    show_line,
+                    html.br(),
+                    html.br(),
+                    "show icon:",
+                    show_icon,
+                    html.br(),
+                    html.br(),
+                ],
+                style=dict(marginBottom=16),
+            ),
+            antd.Tree(
+                showLine=show_line,
+                showIcon=show_icon,
+                defaultExpandedKeys=["0-0-0"],
+                treeData=treeData,
+            ),
+        ]
+    )
