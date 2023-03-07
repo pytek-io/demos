5a3a41a6af5568ce4cda169fb539db84dd2607b0
fdv1 <francois@pytek.io>
2023-03-07 19:12:06 +0000

    ant demos minor fixes

--- ant/demos/data_entry/date-picker/disabled_date.py
+++ ant/demos/data_entry/date-picker/disabled_date.py
@@ -1,31 +1,30 @@
 import datetime
 
-import reflect as r
 import reflect_antd as antd
 
 RangePicker = antd.DatePicker.RangePicker
 
 
 disabledDate = r.js_arrow(
-    "disabledDate", "current => current && current < dayjs().endOf('day')"
+    "disabledDate",
+    """(current) => current && current < dayjs().endOf('day')""",
 )
 
 disabledDateTime = r.js_arrow(
     "disabledDateTime",
-    """
-  () => {
-  range = (start, end) => {
-    result = [];
-    for (let i = start; i < end; i++) {
-      result.push(i);
-    }
-    return result;
-  };
-  return {
-    disabledHours: () => range(0, 24).splice(4, 20),
-    disabledMinutes: () => range(30, 60),
-    disabledSeconds: () => [55, 56],
-  };
+    """() => {
+    const range = (start, end) => {
+        const result = [];
+        for (let i = start; i < end; i++) {
+            result.push(i);
+        }
+        return result;
+    };
+    return {
+        disabledHours: () => range(0, 24).splice(4, 20),
+        disabledMinutes: () => range(30, 60),
+        disabledSeconds: () => [55, 56],
+    };
 };
 """,
 )
